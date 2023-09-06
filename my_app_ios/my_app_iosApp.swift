54dd37e7e4000d64322bc4202c645a39f5121f8e
luis palomino <lpalominomontes@hotmail.com>
2023-09-06 19:25:30 -0300

    Initial Commit

---
+++ my_app_ios/my_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_app_iosApp.swift
+//  my_app_ios
+//
+//  Created by luis palomino on 06-09-23.
+//
+
+import SwiftUI
+
+@main
+struct my_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
